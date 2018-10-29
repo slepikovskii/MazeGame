61ef0c71bb58c6e546d8c886ca507fb897eb65f7
slepikovskii <43961677+slepikovskii@users.noreply.github.com>
2018-10-29 17:52:14 +0200

    Initial commit

---
+++ Assets/Materials/BulletMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour {
+
+	//Members
+	public float speed =7.0f;
+	public float maxDistance =0.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.forward * speed * Time.deltaTime);
+		maxDistance += 1 * Time.deltaTime;
+		if(maxDistance >= 1)
+		{
+			Destroy(this
+	}
+}
